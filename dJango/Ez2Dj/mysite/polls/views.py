806bf0c80da40f5b2fb7be8c45bd463e8c1fb064
sj <bcj0114@gmail.com>
2021-12-28 16:38:15 +0900

    dJango day2

--- dJango/Ez2Dj/mysite/polls/views.py
+++ dJango/Ez2Dj/mysite/polls/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    response = " You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're boing on question %s." % question_id)
